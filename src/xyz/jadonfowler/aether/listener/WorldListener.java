4a141760656945f15911d6951af62e4a11035bdb
Phase <ph4sesaber@gmail.com>
2015-10-12 13:11:09 -0700

    Fall from Aether to normal world

--- src/xyz/jadonfowler/aether/listener/WorldListener.java
+++ src/xyz/jadonfowler/aether/listener/WorldListener.java
@@ -10,35 +10,39 @@ import xyz.jadonfowler.aether.*;
 
 public class WorldListener implements Listener {
 
-	@EventHandler
-	public void eatCoal(PlayerInteractEvent e) {
-		Player p = e.getPlayer();
-		if (Aether.isInAether(p)) {
-			if (p.getFoodLevel() > 19)
-				return;
-			if (p.getItemInHand().getType() == Material.COAL) {
-			    ItemStack hand = p.getItemInHand();
-				if (p.getItemInHand().getAmount() > 1)
-	                p.setItemInHand(new ItemStack(hand.getType(), hand.getAmount() - 1));
-				else
-					p.setItemInHand(new ItemStack(Material.AIR, 1));
-				p.getWorld().playSound(p.getLocation(), Sound.BURP, 3, 1);
-				p.setFoodLevel(p.getFoodLevel() + 5);
-			}
-		}
-	}
+    @EventHandler public void fallInVoid(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        if (Aether.isInAether(p)) {
+            if (e.getTo().getY() <= 10) {
+                p.teleport(new Location(Bukkit.getWorld("world"), e.getTo().getX(), 315, e.getTo().getZ()));
+            }
+        }
+    }
 
-	@EventHandler
-	public void mineZanite(BlockBreakEvent e) {
-		Player p = e.getPlayer();
-		if (Aether.isInAether(p)) {
-			if (e.getBlock().getType() == Material.IRON_ORE) {
-				e.setCancelled(true);
-				e.getBlock().setType(Material.AIR);
-				e.getBlock().getLocation().getWorld()
-					.dropItemNaturally(e.getBlock().getLocation(),
-						new ItemStack(Material.IRON_INGOT, 1));
-			}
-		}
-	}
+    @EventHandler public void eatCoal(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        if (Aether.isInAether(p)) {
+            if (p.getFoodLevel() > 19) return;
+            if (p.getItemInHand().getType() == Material.COAL) {
+                ItemStack hand = p.getItemInHand();
+                if (p.getItemInHand().getAmount() > 1)
+                    p.setItemInHand(new ItemStack(hand.getType(), hand.getAmount() - 1));
+                else p.setItemInHand(new ItemStack(Material.AIR, 1));
+                p.getWorld().playSound(p.getLocation(), Sound.BURP, 3, 1);
+                p.setFoodLevel(p.getFoodLevel() + 5);
+            }
+        }
+    }
+
+    @EventHandler public void mineZanite(BlockBreakEvent e) {
+        Player p = e.getPlayer();
+        if (Aether.isInAether(p)) {
+            if (e.getBlock().getType() == Material.IRON_ORE) {
+                e.setCancelled(true);
+                e.getBlock().setType(Material.AIR);
+                e.getBlock().getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(),
+                        new ItemStack(Material.IRON_INGOT, 1));
+            }
+        }
+    }
 }
